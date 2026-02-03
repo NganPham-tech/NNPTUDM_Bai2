06aac06f125fb88bc3c08675b6ba237a0b7821f8
Ngân <phamthiaingan1205@gmail.com>
2026-02-03 09:05:00 +0700

    sua

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function Product (id, name, price, quantity, category, isAvailable){
     this.category = category;
     this.isAvailable = isAvailable;
 } 
+
+
 let product1 = new Product(1, "Laptop Acer Gaming", 12000000, 10, "Đồ điện tử", true);
 let product2 = new Product(2, "Iphone 17 pro max", 40000000, 25, "Đồ điện tử", true);
 let product3 = new Product(3, "Bàn gaming", 20000000, 5, "Đồ nội thất", false);
@@ -15,6 +17,10 @@ let product6 = new Product(6, "Bộ bàn ghế văn phòng", 3000000, 5, "Đồ 
 let product7 = new Product(7, "Full box Bút Thiên Long", 100000, 5, "Văn phòng phẩm", true);
 let product8 = new Product(8, "Đèn dát vàng xuất xứ Italy", 99999999, 2, "Đồ nội thất", true);
 
+
+let products = [product1, product2, product3, product4, product5, product6, product7, product8];
+
+
 let mang = products.map(function(item) {
     return {
         name: item.name,
@@ -22,31 +28,40 @@ let mang = products.map(function(item) {
     };
 });
 console.log("Câu 3:", mang);
-console.log("Câu 3 - Mang moi", mang);
-let conHangTrongKho = products.filter(item => item.quantity > 0);
-console.log("Câu 4 - San pham con hang: ", conHangTrongKho);
-let sanPhamTren30 = products.some(item => item.price > 30000000);
-console.log("Câu 5 - San pham tren 30 trieu: ", sanPhamTren30);
 
-let phukien = products.filter(item => item.category === "Accessories");
+
+let conHangTrongKho = products.filter(item => item.quantity > 0);
+console.log("Câu 4 - Sản phẩm còn hàng: ", conHangTrongKho);
+
+
+let sanPhamTren30 = products.some(item => item.price > 30000000);
+console.log("Câu 5 - Có sản phẩm trên 30 triệu: ", sanPhamTren30);
+
+
+let phukien = products.filter(item => item.category === "Đồ điện tử");
 let phukiendangban = phukien.every(item => item.isAvailable === true);
-console.log("Câu 6 -Tất cả phụ kiện đang bán:", phukiendangban);
+console.log("Câu 6 - Tất cả sản phẩm Đồ điện tử đang bán:", phukiendangban);
+
+
 let totalGiaTriHangHoa = products.reduce((total, item) => {
     return total + (item.price * item.quantity);
 }, 0);
-console.log("Câu 7- Tổng giá trị hàng hóa trong kho:", totalGiaTriHangHoa);
+console.log("Câu 7 - Tổng giá trị hàng hóa trong kho:", totalGiaTriHangHoa);
 
-console.log("Câu 8 -Danh sách sản phẩm:");
+
+console.log("Câu 8 - Danh sách sản phẩm:");
 for (let item of products) {
     console.log(`${item.name} - ${item.category} - ${item.isAvailable ? "Đang bán" : "Ngừng bán"}`);
 }
 
+
 console.log("Câu 9 - Chi tiết sản phẩm đầu tiên:");
 let firstProduct = products[0];
 for (let key in firstProduct) {
-   
     console.log(`${key}: ${firstProduct[key]}`);
 }
+
+
 let sellingNames = products
     .filter(item => item.isAvailable && item.quantity > 0)
     .map(item => item.name);
